a5c887afdf39cf587438d9049493c496db5f8c73
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-01-18 12:10:15 +0400

    19 documentation (#20)
    * docs
    
    * docs

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -5,6 +5,9 @@ from tinycta.linalg import a_norm, inv_a_norm, solve, valid
 
 
 def test_non_quadratic():
+    """
+    Mismatch in matrix not square
+    """
     with pytest.raises(AssertionError):
         valid(np.array([[2.0, 1.0]]))
 
@@ -19,6 +22,9 @@ def test_non_quadratic():
 
 
 def test_mismatch():
+    """
+    Mismatch in matrix dimensions vs vector dimensions
+    """
     with pytest.raises(AssertionError):
         a_norm(vector=np.array([1.0, 2.0]), matrix=np.array([[1.0]]))
 
@@ -30,6 +36,9 @@ def test_mismatch():
 
 
 def test_valid():
+    """
+    Valid submatrices
+    """
     a = np.array([[1.0, np.NaN], [np.NaN, np.NaN]])
     val, submatrix = valid(a)
 
@@ -38,6 +47,9 @@ def test_valid():
 
 
 def test_valid_eye():
+    """
+    Valid submatrix applied to identity
+    """
     a = np.eye(2)
     val, submatrix = valid(a)
 
@@ -46,40 +58,61 @@ def test_valid_eye():
 
 
 def test_anorm_without_matrix():
+    """
+    a-norm without matrix
+    """
     v = np.array([[3.0, 4.0]])
     assert a_norm(vector=v) == pytest.approx(5.0)
 
 
 def test_anorm_with_matrix():
+    """
+    a-norm with matrix
+    """
     v = np.array([3.0, 4.0])
     a = 2 * np.eye(2)
     assert a_norm(vector=v, matrix=a) == pytest.approx(np.sqrt(2) * 5.0)
 
 
 def test_anorm_all_nan():
+    """
+    a-norm with matrix but all NaNs
+    """
     v = np.array([3.0, 4.0])
     a = np.NaN * np.eye(2)
     assert np.isnan(a_norm(vector=v, matrix=a))
 
 
 def test_inv_a_norm():
+    """
+    inv a-norm
+    """
     v = np.array([3.0, 4.0])
     a = 0.5 * np.eye(2)
     assert inv_a_norm(vector=v, matrix=a) == pytest.approx(np.sqrt(2) * 5.0)
 
 
 def test_inv_a_norm_without_matrix():
+    """
+    inv-a-norm without matrix a
+    """
     v = np.array([3.0, 4.0])
     assert inv_a_norm(vector=v) == pytest.approx(5.0)
 
 
 def test_inv_a_norm_all_nan():
+    """
+    inv-a-norm with matrix a but all NaNs
+    """
     v = np.array([3.0, 4.0])
     a = np.NaN * np.eye(2)
     assert np.isnan(inv_a_norm(vector=v, matrix=a))
 
 
 def test_solve():
+    """
+    test solve
+    """
     rhs = np.array([3.0, 4.0])
     matrix = 0.5 * np.eye(2)
     x = solve(matrix=matrix, rhs=rhs)
