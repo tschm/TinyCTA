d8986123a04420f1c7fe9025a81f0c478f6372d8
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-05-14 23:01:04 -0700

    testing monthtable

--- tests/test_experiment1.py
+++ tests/test_experiment1.py
@@ -4,7 +4,7 @@ import pytest
 import quantstats as qs
 
 from tinycta.port import build_portfolio
-
+from tinycta.month import monthlytable
 
 # take two moving averages and apply sign-function
 def f(prices, fast=32, slow=96):
@@ -29,3 +29,12 @@ def test_portfolio(prices):
     assert qs.stats.sharpe(
         portfolio.truncate(before=portfolio.start).profit
     ) == pytest.approx(0.5548581162109552)
+
+
+def test_month(prices):
+    portfolio = build_portfolio(prices=prices, cashposition=1e6 * f(prices))
+    returns = portfolio.profit / portfolio.profit.std()
+
+    table = 100*monthlytable(portfolio.nav(aum=1e6).pct_change())
+    assert table["YTD"].loc[1973] == pytest.approx(231.72540665897102)
+
